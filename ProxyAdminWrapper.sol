7e63fd71f1f8035330c7447825cb41de472103df
Dimeh63 <104158864+Dimeh63@users.noreply.github.com>
2023-08-14 17:04:22 +0200

    Update ProxyAdminWrapper.sol

--- ProxyAdminWrapper.sol
+++ ProxyAdminWrapper.sol
@@ -1,10 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
+// Importation du contrat ProxyAdmin d'OpenZeppelin.
+// ProxyAdmin est un contrat qui sert d'administrateur pour les proxies transparents.
 import "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
 
+// Le contrat ProxyAdminWrapper hérite de ProxyAdmin.
 contract ProxyAdminWrapper is ProxyAdmin {
+
+    // Le constructeur prend un paramètre :
+    // initialOwner : l'adresse du propriétaire initial du contrat ProxyAdmin.
     constructor(address initialOwner) ProxyAdmin() {
+        // Transfert de la propriété du contrat ProxyAdmin à l'adresse fournie.
         transferOwnership(initialOwner);
     }
 }
+
