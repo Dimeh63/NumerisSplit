b2f0bb15878e9d4f5ff6b57d777d967b385fb0cf
Dimeh63 <104158864+Dimeh63@users.noreply.github.com>
2023-08-14 16:24:36 +0200

    Update FeeRedistributionContract.sol

--- FeeRedistributionContract.sol
+++ FeeRedistributionContract.sol
@@ -11,6 +11,10 @@ contract FeeRedistributionContract is BaseContract {
     mapping(address => uint256) public balances;
     PAXGImplementation private paxgTokenInstance;
 
+    // Ajout d'événements
+    event BalanceUpdated(address indexed user, uint256 amount);
+    event FeesClaimed(address indexed user, uint256 amount);
+
     function initialize(address _paxgToken) public initializer onlyOwner {
         super.initialize();
         paxgTokenInstance = PAXGImplementation(_paxgToken);
@@ -19,12 +23,17 @@ contract FeeRedistributionContract is BaseContract {
     function updateBalance(address user, uint256 amount) external {
         require(msg.sender == address(paxgTokenInstance), "Only PAXGImplementation can update balance");
         balances[user] = balances[user].add(amount);
+        emit BalanceUpdated(user, amount); // Émettre un événement lors de la mise à jour du solde
     }
 
     function claimFees() external {
         uint256 amount = balances[msg.sender];
         require(amount > 0, "No fees to claim");
+        // Ajout d'une limite pour éviter des retraits massifs
+        require(amount <= 100 ether, "Maximum withdrawal limit exceeded");
         balances[msg.sender] = 0;
         require(paxgTokenInstance.transfer(msg.sender, amount), "Transfer failed");
+        emit FeesClaimed(msg.sender, amount); // Émettre un événement lors de la réclamation des frais
     }
 }
+
