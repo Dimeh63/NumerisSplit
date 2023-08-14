dcee9cc86a8817918b680b19d687c76436b6008b
Dimeh63 <104158864+Dimeh63@users.noreply.github.com>
2023-08-14 17:13:26 +0200

    Update BaseContract.sol

--- BaseContract.sol
+++ BaseContract.sol
@@ -3,8 +3,19 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
+/**
+ * @title BaseContract
+ * @dev Ce contrat sert de base pour d'autres contrats en intégrant la fonctionnalité "Ownable" d'OpenZeppelin.
+ * Il est conçu pour être utilisé avec le modèle de mise à niveau d'OpenZeppelin, où les contrats peuvent être mis à niveau sans perdre l'état.
+ */
 contract BaseContract is Initializable, OwnableUpgradeable {
+
+    /**
+     * @dev Fonction d'initialisation qui configure le propriétaire du contrat.
+     * Elle utilise le modificateur 'initializer' pour s'assurer qu'elle ne peut être appelée qu'une seule fois.
+     */
     function initialize() public virtual initializer {
         __Ownable_init();
     }
 }
+
